6f96aed5d86a0cd2bf81530b06ec8704a8d82839
muhammadAli289 <muhammad.ali@senarios.live>
2024-12-05 22:47:10 +0500

    auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,18 +7,25 @@ export default function AuthLayout({
   }) {
     return (
       <div className="flex min-h-screen">
-        <section className="bg-brand p-10">
-            <div>
-            <Image src="/logo.png" width={224} height={84} alt="" className="h-auto" />
+        <section className="hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5">
+            <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
+            <Image src="/assets/icons/logo-full.svg" width={224} height={84} alt="" className="h-auto" />
             <div className="space-y-5 text-white">
                 <h1 className="h1">Manage Your file the best way</h1>
                 <p className="body-1">This is place where you can store all your document</p>
             </div>
-            <Image src="/illustration.png" width={342} height={342} alt="" className="transition-all hover:rotate-2 hover:scale-105"  />
+            <Image src="/assets/images/files.png" width={342} height={342} alt="" className="transition-all hover:rotate-2 hover:scale-105"  />
 
             </div>
         </section>
+        <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0 ">
+          <div className="mb-16 lg:hidden">
+            
+            <Image src="/assets/icons/logo-full-brand.svg" height={84} width={224} className="h-auto w-[200px] lg:w-[250px]" alt="test"/>
+            </div>
+
         {children}
+        </section>
       </div>
     );
   }
